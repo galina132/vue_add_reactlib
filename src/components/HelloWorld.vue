762a1eafafbbaa95e6ed8be1664292c98ff233cb
galina132 <gala.bragina2014@yandex.ru>
2019-10-29 00:19:05 +0300

    lib griz

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <griz>
+      <griz-col>Column 1</griz-col>
+      <griz-col>Column 2</griz-col>
+    </griz>
+  </div>
+</template>
+
+<script>
+import { Grid, GridCol } from 'griz'
+export default {
+  name: 'HelloWorld',
+  components: { Grid, GridCol },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
